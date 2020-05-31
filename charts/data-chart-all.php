d05e8d96c399ccad6af15f0bbae06f7fd8062f8c
tnahimana <38537362+tnahimana@users.noreply.github.com>
2020-05-31 21:48:04 +0200

    Update data-chart-all.php

--- charts/data-chart-all.php
+++ charts/data-chart-all.php
@@ -1,8 +1,7 @@
 <?php
 header('Content-Type: application/json');
 
-$conn = mysqli_connect("localhost","root","kigali@kigali","restaurent_management");
-
+$conn = mysqli_connect("142.93.41.188","dqwawsnkgn","h95GKy8KWq","dqwawsnkgn");
 
 // $sqlQuery = "SELECT user_fname FROM users_activity ORDER BY register_date DESC LIMIT 10";
 $sqlQuery = "SELECT count(*) as marks, user_username  FROM users_activity group by user_username  ASC";
